b48703f88c0e869017caec83a5836072fa03df08
Aileen Thai <thai@osuosl.org>
2016-05-18 16:37:56 -0700

    Experimenting

--- streamwebs_frontend/streamwebs/admin.py
+++ streamwebs_frontend/streamwebs/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
 from .models import UserProfile
 
-# Register your models here.
+# The following will add a profile model's files to the user page in the
+# admin panel
 
-admin.site.register(UserProfile)
+# Define an inline admin descriptor for normal profile model
+# which acts a bit like a singleton
+# refed from: https://docs.djangoproject.com/en/1.9/topics/auth/customizing/
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+  inlines = (UserProfileInline, )
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+#admin.site.register(UserProfile)
