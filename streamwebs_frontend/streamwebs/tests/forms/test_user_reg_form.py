0ac17ceac07736ce0aae319df7e56766d1800cb1
leian7 <leian@osuosl.org>
2016-05-25 13:12:35 -0700

    Delete comment

--- streamwebs_frontend/streamwebs/tests/forms/test_user_reg_form.py
+++ streamwebs_frontend/streamwebs/tests/forms/test_user_reg_form.py
@@ -1,10 +1,6 @@
 from django.test import TestCase
 
 from streamwebs.forms import UserForm, UserProfileForm
-from django.contrib.gis.db import models
-from django import forms
-from django.apps import apps
-from itertools import chain
 
 
 class UserFormTestCase(TestCase):
@@ -13,8 +9,6 @@ class UserFormTestCase(TestCase):
         self.expected_fields = ('username', 'email', 'password', 'first_name', 'last_name')
 
     def test_UserForm_fields_exist(self): 
-        #create UserForm object
-        #does it have such-and-such attribute/field????
         user_form = UserForm()
         for i in range(len(self.expected_fields)):
             self.assertEqual(user_form.Meta.fields[i], self.expected_fields[i])
