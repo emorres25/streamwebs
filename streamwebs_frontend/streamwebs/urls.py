fbd69a0dddbafa5ca86b566252f79acdd166007b
leian7 <leian@osuosl.org>
2016-05-25 13:26:21 -0700

    Map registration url

--- streamwebs_frontend/streamwebs/urls.py
+++ streamwebs_frontend/streamwebs/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'streamwebs'
 urlpatterns = [
   url(r'^$', views.index, name='index'),
+  url(r'^register/$', views.register, name='register'),
 ]
