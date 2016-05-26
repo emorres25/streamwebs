8ba2a6bba3d6234cc0864953b16ff3911e01facf
leian7 <leian@osuosl.org>
2016-05-26 15:57:11 -0700

    Add register view

--- streamwebs_frontend/streamwebs/views.py
+++ streamwebs_frontend/streamwebs/views.py
@@ -3,16 +3,42 @@ from django.template.loader import render_to_string
 from django.http import HttpResponse
 from django.template import RequestContext
 
+from streamwebs.forms import UserForm, UserProfileForm
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hey, you've reached the index.")
 
 def register(request):
     context = RequestContext(request)
-    registered = True
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+            registered = True
+        
+        else:
+            print user_form.errors, profile_form.errors
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
 
     return render_to_response(
-        'streamwebs/register.html',
-        {'registered': registered}, context)
-
+            'streamwebs/register.html',
+            {'user_form': user_form, 'profile_form': profile_form, 'registered': registered}, context)
+    
+#    return render_to_response(
+#        'streamwebs/register.html',
+#        {'registered': registered}, context)
+ 
  
