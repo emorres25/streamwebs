a4b4d935da8b850ceed1cc1d1c0208cc7283e847
leian7 <leian@osuosl.org>
2016-05-26 13:21:14 -0700

    Add setUp to instantiate test client

--- streamwebs_frontend/streamwebs/tests/views/test_registrate_view.py
+++ streamwebs_frontend/streamwebs/tests/views/test_registrate_view.py
@@ -1,16 +1,18 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 from django.core.urlresolvers import reverse
 
 from streamwebs.forms import UserForm, UserProfileForm
 
 
 class RegistrateTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
     
     def test_view_with_registered_user(self):
         """
         When the user has been registered, they should see a "thanks for registering" page
         """
- #       self.registered == True
+        self.registered = False
         response = self.client.get(reverse('streamwebs:register'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "You have successfully created a StreamWebs account.")
